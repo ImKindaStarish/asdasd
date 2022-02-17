162505944ab1c1826a5cf6893ec19d5398b6c7ab
Par. B <94152919+TheGuy888@users.noreply.github.com>
2022-02-17 10:35:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -10586,7 +10586,7 @@ Game.Launch=function()
 			building.productionAchievs.push({pow:pow,achiev:achiev});
 			return achiev;
 		}
-		
+		Game.win('Third-Party');
 		Game.thresholdIcons=[0,1,2,3,4,5,6,7,8,9,10,11,18,19,20,21,22,23,24,25,26,27,28,29,21,22,23,24,25,26,27,28,29,21,22,23,24,25,26,27,28,29];
 		Game.BankAchievements=[];
 		Game.BankAchievement=function(name,q)
